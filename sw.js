4e96ee4e96fb79571410f07bd49111debe74b1b6
gonzahag <gonzahag@gmail.com>
2026-08-08 04:19:02 -0300

    initial commit

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'ruleta-watchlist-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  // Solo servimos desde caché lo que es parte de la app (mismo origen).
+  // Todo lo demás (TMDB, JustWatch, Letterboxd) siempre va directo a la red.
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
